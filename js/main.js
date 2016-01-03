a7363298a65e34ab0a9b353564b5cd117fec0579
Tatiana <tatiana.melennaya@gmail.com>
2016-01-03 12:12:54 +0700

    debris removal

--- js/main.js
+++ js/main.js
@@ -32,40 +32,7 @@
     });
 
     setHeight();
-    // local scroll
-   // jQuery('.navbar').localScroll({hash: true, offset: {top: 0}, duration: 800, easing: 'easeInOutExpo'});
 
-    // YouTube
-    //  if ($(window).width() >= 977) {
-    //     $('#slider-wrap .video').YTPlayer({
-    //         fitToBackground: false,
-    //         videoId: 'vzyXKT9OFxk',
-    //         pauseOnScroll: false,
-    //         playerVars: {
-    //             modestbranding: 0,
-    //             autoplay: 1,
-    //             controls: 0,
-    //             showinfo: 0,
-    //             wmode: 'transparent',
-    //             branding: 0,
-    //             rel: 0,
-    //             autohide: 0,
-    //             origin: window.location.origin
-    //         }
-    //     });
-    // }
-
-    $.fn.YTPlayer = function(options) {
-
-        return this.each(function() {
-            var el = this;
-
-            $(el).data("yt-init", true);
-            var player = Object.create(YTPlayer);
-            player.init(el, options);
-            $.data(el, "ytPlayer", player);
-        });
-    };
 
     // portfolio
     if ($('.isotopeWrapper').length) {
@@ -107,41 +74,6 @@
             });
         });
     }
-
-
-    //height columns
-
-    //$('#features .number-wrap').each(function () {
-    //    var $this = $(this),
-    //        height = $this.outerHeight(),
-    //        $next = $this.next(),
-    //        $prev = $this.prev();
-    //
-    //    console.log($this,height);
-    //
-    //    if ($this.hasClass('first')) {
-    //        $next.css({
-    //            'height': height
-    //        });
-    //        $next.find('.wrap').css({
-    //            'margin-top': -$next.find('.wrap').outerHeight() / 2 + 'px'
-    //        });
-    //    } else {
-    //        $prev.css('height', height);
-    //        $prev.find('.wrap').css({
-    //            'margin-top': -$prev.find('.wrap').outerHeight() / 2 + 'px'
-    //        });
-    //    }
-    //});
-
-
-    //
-    //function setHeight() {
-    //    var clientHeight = $(window).height();
-    //    $('#main-slider-wrap').css('height', clientHeight);
-    //}
-    //
-    //setHeight();
 
     //main-slider
     var slick = $('#main-slider-wrap').slick({
